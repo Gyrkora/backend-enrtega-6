3716c6d2dda5e22b9e436cfa284beeec1294b6aa
Gyrkora <giselle.gallardo0210@gmail.com>
2022-07-24 00:04:38 -0300

    desafio incompleto

---
+++ public/js/index.js
@@ -0,0 +1,94 @@
+// cliente socket.io
+const socket = io()
+
+// const productContainer = document.querySelector('#productContainer')
+const titleProd = document.getElementsByClassName('titulo').value
+const priceProd = document.getElementsByClassName('precio').value
+const imgProd = document.getElementsByClassName('foto').value
+const body = document.querySelector('body')
+
+//formulario de ingreso de productos
+const productForm = $('#formNewProduct')
+//la tabla del output
+const productContainer = $('#productContainer')
+
+socket.emit('productRequest')
+
+productForm.submit((event) => {
+	event.preventDefault()
+
+	const newProduct = {
+		title: productForm[0][0].value,
+		price: productForm[0][1].value,
+		foto: productForm[0][2].value,
+	}
+
+	socket.emit('addProd', newProduct)
+	productForm.trigger('reset')
+})
+
+function renderProd(data) {
+	const html = data
+		.map((elem) => {
+			return `<div>
+            <strong>${elem.title}</strong>:
+            <em>${elem.price}</em> </div>`
+		})
+		.join(' ')
+	productContainer.append(html)
+}
+
+socket.on('saveProd', (data) => {
+	renderProd(data)
+})
+
+// function addProd(e) {
+// 	const newProd = {
+// 		// title: titleProd ? titleProd : 'No Title',
+// 		// price: priceProd ? priceProd : 0,
+// 		// thumbnail: imgProd ? imgProd : 'No image',
+
+// 		title: document.getElementById('titulo').value,
+// 		price: document.getElementById('precio').value,
+// 		thumbnail: document.getElementById('foto').value,
+// 	}
+
+// 	socket.emit('new-Prod', newProd)
+// 	console.log(newProd)
+// 	return false //e.prevent.default
+// }
+
+// async function productListHandler(getAll) {
+// 	const productLayout = await fetch('productos.handlerbars')
+// 	const layoutText = await productLayout.text()
+// 	const compiledHbsTemplate = Handlebars.compile(layoutText)
+// 	const html = compiledHbsTemplate({ getAll })
+// 	productContainer.empty().append(html)
+// }
+
+// socket.on('saveProd', productListHandler)
+
+// const newProd = {
+// 	title: titleProd,
+// 	precio: priceProd,
+// 	foto: ImgProd,
+// }
+
+// productContainer2.innerHTML = JSON.stringify(newProd)
+// console.log(typeof newProd.title)
+
+// function addProd() {
+// 	// event.preventDefault()
+
+// 	const newProd = {
+// 		title: titleProd.value,
+// 		precio: priceProd.value,
+// 		foto: ImgProd.value,
+// 	}
+
+// 	productContainer2.innerHTML = newProd.title
+
+// 	socket.emit('new-Prod', newProd)
+// 	productForm.trigger('reset')
+// 	return false
+// }
---
+++ src/server.js
@@ -0,0 +1,51 @@
+import express from 'express'
+import { router } from './routes.js'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import { Contenedor, productsList } from './productos.js'
+import cors from 'cors'
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+//# varibles generales
+const products = new Contenedor(productsList)
+
+//# MWs
+const PORT = 8080
+const __dirname = process.cwd()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/', router)
+app.use(cors())
+
+app.use(express.static('../public'))
+app.get('/', (req, res) => {
+	res.sendFile('inicio', { root: __dirname })
+})
+
+//# Engine
+app.set('view engine', 'ejs')
+
+//# views
+app.set('views', '../public')
+
+io.on('connection', (socket) => {
+	console.log('usuario connected !')
+
+	socket.on('productRequest', async () => {
+		const getAll = await products.getAll()
+		socket.emit('saveProd', getAll) //manda data
+	})
+
+	socket.on('addProd', async (data) => {
+		await products.save(data)
+		const allProds = await products.getAll()
+		io.sockets.emit('saveProd', allProds)
+	})
+})
+
+//# levantando
+httpServer.listen(PORT, () => console.log('SERVER UP'))
